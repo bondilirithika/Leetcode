8dc4b98e1532abb8e13f55defcc0c52363cbcc43
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2024-12-05 13:10:50 +0530

    Time: 23 ms (39.16%) | Memory: 63 MB (33.98%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -6,40 +6,19 @@ class Solution {
         {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-
+        int op=0;
         for(int k:map.keySet())
         {
+
             int a=map.get(k);
-            if(a==2)
-            count+=1;
             if(a<2)
             return -1;
-            if(a>2)
+            op+=a/3;
+            if(a%3>0)
             {
-                int div=a/3;
-                int rem=a%3;
-                if(rem==0)
-                {
-                    count+=div;
-                }
-                else if(rem == 1)
-                {
-                    count+=div-1;
-                    rem=a-(div-1)*3;
-                    if(rem%2==0)
-                    {
-                        count+=rem/2;
-                    }
-                }
-                else
-                {
-                    count+=div;
-                    rem=a-(div)*3;
-                    count+=rem/2;
-                }
+                op++;
             }
-
         }
-        return count;
+        return op;
     }
 }
