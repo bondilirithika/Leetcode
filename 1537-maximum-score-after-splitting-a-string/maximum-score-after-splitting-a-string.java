664cc07045b7024b3fb71afd29680973e0182fdb
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-01-01 22:21:50 +0530

    Time: 2 ms (39.66%) | Memory: 42 MB (12.87%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,26 +1,30 @@
 class Solution {
     public int maxScore(String s) {
+        //prefix -suffix array
+        int[] zero=new int[s.length()];
+        int[] one=new int[s.length()];
+        int z=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='0')
+            {
+                z++;
+            }
+            zero[i]=z;
+        }
+        int o=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)=='1')
+            {
+                o++;
+            }
+            one[i]=o;
+        }
         int ans=0;
         for(int i=0;i<s.length()-1;i++)
         {
-            String z=s.substring(0,i+1);
-            String o=s.substring(i+1,s.length());
-            int k=0;
-            for(char c:z.toCharArray())
-            {
-                if(c=='0')
-                {
-                    k++;
-                }
-            }
-            for(char c:o.toCharArray())
-            {
-                if(c=='1')
-                {
-                    k++;
-                }
-            }
-            ans=Math.max(ans,k);
+            ans=Math.max(ans,zero[i]+one[i+1]);
         }
         return ans;
     }
