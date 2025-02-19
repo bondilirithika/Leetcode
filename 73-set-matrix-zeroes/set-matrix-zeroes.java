77227f4a869a183d236515cf81458002c1cb81b9
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-02-19 22:09:31 +0530

    Time: 3 ms (7.89%) | Memory: 46 MB (16.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,38 +1,30 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        ArrayList<pair> arr=new ArrayList<>();
+        //see which rows and cols are to be made zero
+        HashSet<Integer> r=new HashSet<>();
+        HashSet<Integer> c=new HashSet<>();
         for(int i=0;i<matrix.length;i++)
         {
             for(int j=0;j<matrix[0].length;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    arr.add(new pair(i,j));
+                    r.add(i);
+                    c.add(j);
                 }
             }
         }
 
-        for(pair p:arr)
+        for(int i=0;i<matrix.length;i++)
         {
-            int row=p.r;
-            int col=p.c;
-            for(int i=0;i<matrix[0].length;i++)
+            for(int j=0;j<matrix[0].length;j++)
             {
-                matrix[row][i]=0;
-            }
-            for(int i=0;i<matrix.length;i++)
-            {
-                matrix[i][col]=0;
+                if(r.contains(i) || c.contains(j))
+                {
+                    matrix[i][j]=0;
+                }
             }
         }
-    }
-}
-class pair
-{
-    int r,c;
-    pair(int r,int c)
-    {
-        this.r=r;
-        this.c=c;
+
     }
 }
