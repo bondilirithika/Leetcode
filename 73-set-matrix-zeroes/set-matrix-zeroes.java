b06c1dde38ef85754069bcb16594c6101e825781
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-02-20 23:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (65.50%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,73 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //see which rows and cols are to be made zero
-        HashSet<Integer> r=new HashSet<>();
-        HashSet<Integer> c=new HashSet<>();
-        for(int i=0;i<matrix.length;i++)
+        boolean row=false;
+        boolean col=false;
+        int r=matrix.length;
+        int c=matrix[0].length;
+        for(int i=0;i<c;i++)
         {
-            for(int j=0;j<matrix[0].length;j++)
+            if(matrix[0][i]==0)
+            {
+                row=true;
+                break;
+            }
+        }
+        for(int i=0;i<r;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                col=true;
+                break;
+            }
+        }
+
+        for(int i=1;i<r;i++)
+        {
+            for(int j=1;j<c;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    r.add(i);
-                    c.add(j);
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
 
-        for(int i=0;i<matrix.length;i++)
+        for(int i=1;i<c;i++)
         {
-            for(int j=0;j<matrix[0].length;j++)
+            if(matrix[0][i]==0)
             {
-                if(r.contains(i) || c.contains(j))
+                for(int j=1;j<r;j++)
+                {
+                    matrix[j][i]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<r;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int j=1;j<c;j++)
                 {
                     matrix[i][j]=0;
                 }
             }
         }
 
+        if(row)
+        {
+            for(int i=0;i<c;i++)
+            {
+                matrix[0][i]=0;
+            }
+        }
+        if(col)
+        {
+            for(int i=0;i<r;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
     }
 }
