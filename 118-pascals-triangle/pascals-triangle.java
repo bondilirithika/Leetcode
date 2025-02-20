36c0df7eeef66554eafae623b73e84abfce22af9
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-02-20 22:05:23 +0530

    Time: 1 ms (84.04%) | Memory: 42 MB (58.33%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,18 +1,27 @@
 class Solution {
     public List<List<Integer>> generate(int n) {
+        //all abt ncr
         List<List<Integer>> arr=new ArrayList<>();
         for(int i=0;i<n;i++)
         {
-            ArrayList<Integer> c=new ArrayList<>();
+            ArrayList<Integer> a=new ArrayList<>();
             for(int j=0;j<i+1;j++)
             {
-                if(j==0 || j==i)
-                c.add(1);
-                else
-                c.add(arr.get(i-1).get(j-1)+arr.get(i-1).get(j));
+                a.add(ncr(i,j));
             }
-            arr.add(c);
+            arr.add(a);
         }
         return arr;
+
+    }
+    int ncr(int n,int r)
+    {
+        int res=1;
+        for(int i=0;i<r;i++)
+        {
+            res=res*(n-i);
+            res=res/(i+1);
+        }
+        return res;
     }
 }
