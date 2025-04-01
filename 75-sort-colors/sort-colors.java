b5d7f722fdaadb8fa2a21798397c46add3e6437e
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-04-01 23:52:54 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (45.75%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,19 +1,36 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums)
+        //3 pointers low mid high
+        // index 0 to low-1->0
+        // indexlow to mid-1->1
+        // mid to high->mixed
+        // index high+1 to n-1->2
+        //since gib=ven is mixed we will take 0to be mid and n-1 to be high nad starting so low will be at 0
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high)
         {
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        int ind=0;
-        for(int i=0;i<=2;i++)
-        {
-            int c=map.getOrDefault(i,0);
-            while(c>0)
+            if(nums[mid]==0)
             {
-                nums[ind++]=i;
-                c--;
+                swap(nums,mid,low);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums,high,mid);
+                high--;
             }
         }
+    }
+
+    void swap(int[] nums,int i,int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
