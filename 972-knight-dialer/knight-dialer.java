06e34c059878f68ee1ce02847a0941aac44036c9
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-04-02 12:57:17 +0530

    Time: 80 ms (34.97%) | Memory: 44.9 MB (37.86%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -1,41 +1,42 @@
 class Solution {
-   // int count;
-     int[][][] dp;
-    int mod;
+    static int mod=1000000007;
+    int[][][] dp;
     public int knightDialer(int n) {
-        int ans = 0;
-        mod = 1000000007;
-        dp = new int[4][3][n];
-        for (int[][] a : dp)
-            for (int[] k : a)
-                Arrays.fill(k, -1);
-        
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3;j++) {
-                if ((i == 3 && j == 0) || (i == 3 && j == 2)) continue;
-                ans = (ans + solve(i, j, n - 1)) % mod;
+        //dont have to store as they idnt ask fo rthe numv=ber just say the no.of valid numbers we can get
+        dp=new int[4][3][n+1];
+        for(int[][] arr:dp)
+        {
+            for(int[] a:arr)
+            {
+                Arrays.fill(a,-1);
             }
         }
-        return ans % mod;
+        int ans=0;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                if(i==3 && (j==0 || j==2))continue;
+                ans=(ans+valid(i,j,n-1)%mod)%mod;
+            }
+        }
+        return ans%mod;
     }
 
-    int solve(int i, int j, int n) {
-        if (i<0||i>3||j<0||j>2) return 0;
-        if ((i==3 && j==0) || (i==3 && j==2)) return 0;
-        if (n==0) return 1;
-        
-        if (dp[i][j][n] != -1) return dp[i][j][n];
-
-        int no = 0;
-        no = (no+solve(i-2, j + 1, n - 1) % mod) % mod;
-        no = (no+solve(i-2, j - 1, n - 1) % mod) % mod;
-        no = (no+solve(i-1, j + 2, n - 1) % mod) % mod;
-        no = (no+solve(i+1, j + 2, n - 1) % mod) % mod;
-        no = (no+solve(i-1, j - 2, n - 1) % mod) % mod;
-        no = (no+solve(i+1, j - 2, n - 1) % mod) % mod;
-        no = (no+solve(i+2, j + 1, n - 1) % mod) % mod;
-        no = (no+solve(i+2, j - 1, n - 1) % mod) % mod;
-        
-        return dp[i][j][n] = no;
+    int valid(int i,int j,int n)
+    {
+        if(i<0 || i>3 || j<0 || j>2 ||(i==3 && (j==0 || j==2))) return 0;
+        if(n==0)return 1;
+        if(dp[i][j][n]!=-1)return dp[i][j][n];
+        int count=0;
+        count=(count+valid(i-1,j-2,n-1))%mod;
+        count=(count+valid(i-1,j+2,n-1))%mod;
+        count=(count+valid(i-2,j-1,n-1))%mod;
+        count=(count+valid(i-2,j+1,n-1))%mod;
+        count=(count+valid(i+1,j-2,n-1))%mod;
+        count=(count+valid(i+1,j+2,n-1))%mod;
+        count=(count+valid(i+2,j-1,n-1))%mod;
+        count=(count+valid(i+2,j+1,n-1))%mod;
+        return dp[i][j][n]=count%mod;
     }
 }
