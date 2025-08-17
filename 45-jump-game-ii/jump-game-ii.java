1d2fb5ac542496f77af11ea58b89bb34c34397ff
Bondili Rithika Singh <168219157+bondilirithika@users.noreply.github.com>
2025-08-17 19:09:31 +0530

    Time: 1 ms (99.53%) | Memory: 45.1 MB (60.65%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,22 +1,20 @@
 class Solution {
     public int jump(int[] nums) {
-        //interval that you can reach
-        int maxreach=nums[0];
-        int startindex=1;
-        int jump=0;
         if(nums.length==1)
         return 0;
+        int maxreach=nums[0];
+        int stind=1;
+        int ju=0;
         while(maxreach<nums.length-1)
         {
             int locmax=maxreach;
-            for(int i=startindex;i<=locmax;i++)
+            for(int i=stind;i<=locmax;i++)
             {
-                int reach=i+nums[i];
-                maxreach=Math.max(maxreach,reach);
+                maxreach=Math.max(maxreach,i+nums[i]);
             }
-            startindex=locmax+1;
-            jump++;
+            stind=locmax+1;
+            ju++;
         }
-        return ++jump;
+        return ju+1;
     }
 }
